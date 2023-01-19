302d509db00e4c2e72fc3a0a8fbcac710a6a7269
Supercat771 <122520268+Supercat771@users.noreply.github.com>
2023-01-19 23:30:51 +0900

    Add files via upload
    1월 20일차 금요일 스터디용 입니다.

---
+++ studygroup/src/java_studyGroup/Main_11654.java
@@ -0,0 +1,30 @@
+package java_studyGroup;
+
+import java.util.Scanner;
+
+public class Main_11654 {  
+
+	public static void main(String[] args) {
+		
+	
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int num = sc.nextLine().charAt(0);
+		
+		
+		
+		System.out.println(num);
+		
+//		char ch = sc.nextLine().charAt(0);
+//		
+//		System.out.println((int)ch);
+		
+			
+		
+		
+	}
+
+}
