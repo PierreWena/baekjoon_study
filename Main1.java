7cbab82ead6d5359e3cb4b29b2676920faad0481
inganwinner <121844125+inganwinner@users.noreply.github.com>
2023-01-20 17:34:34 +0900

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,21 @@
+package backjun;
+import java.util.Scanner;
+public class Main1 {
+
+
+	
+
+		public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String S = sc.nextLine();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(char i='a'; i<='z'; i++) {
+			sb.append(S.indexOf(i) + " ");
+		}
+		System.out.println(sb.toString());
+		}
+	}
