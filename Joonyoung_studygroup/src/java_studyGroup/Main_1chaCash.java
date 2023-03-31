01cc4cf7e23b9a91818623168a6c84ccf7ebed5c
Supercat771 <122520268+Supercat771@users.noreply.github.com>
2023-03-31 10:05:33 +0900

    Update Main_1chaCash.java

--- Joonyoung_studygroup/src/java_studyGroup/Main_1chaCash.java
+++ Joonyoung_studygroup/src/java_studyGroup/Main_1chaCash.java
@@ -2,7 +2,7 @@ package myalgorithm;
 
 // 
 
-//문제출처 :  https://school.programmers.co.kr/learn/courses/30/lessons/17680
+//출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/courses/30/lessons/17680
 
 import java.util.*;
 
